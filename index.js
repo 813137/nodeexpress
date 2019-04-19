94a13179462b63e353632bb1c7ebcebc1f3471e2
knowledge <knowledgedzumba@gmail.com>
2019-04-19 08:17:43 +0200

    changed port number settings to accommodate azure

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let express = require('express')
 let mainRouter = require('./mainRoutes')
 let app = express()
+let port = process.env.PORT || 3000
 
 app.use(mainRouter)
 
-app.listen(3000)
-console.log('Express server running on port 3000')
+app.listen(port)
+console.log('Express server running on port ', port)
