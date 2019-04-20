1e9ad2a0b6871d59b94850146551aefa461617ba
knowledge <knowledgedzumba@gmail.com>
2019-04-20 11:37:59 +0200

    made application aware of which port to use

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ let mainRouter = require('./mainRoutes')
 let app = express()
 
 app.use(mainRouter)
-app.listen(3000)
+let port = process.env.PORT || 3000
+app.listen(port)
 console.log('Express server running on port 3000')
