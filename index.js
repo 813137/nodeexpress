2bd09012d11ef7358e510ba552d4afca8a206956
knowledge <knowledgedzumba@gmail.com>
2019-04-20 11:15:26 +0200

    created a module for routing HTTP requests

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 'use strict'
 
-let path = require('path')
+// Import the necessary modules for routing and path manipulation
 let express = require('express')
+let mainRouter = require('./mainRoutes')
 let app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-app.get('/about', function (req, res) {
-  res.sendFile(path.join(__dirname, 'views', 'about.html'))
-})
-
+app.use(mainRouter)
 app.listen(3000)
 console.log('Express server running on port 3000')
