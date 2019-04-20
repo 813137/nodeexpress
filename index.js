ccfcd0c19ecebd96487741ddc630f8385c67059f
knowledge <knowledgedzumba@gmail.com>
2019-04-20 10:54:46 +0200

    Create about page and a route for it

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listed(3000)
+app.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
+app.listen(3000)
 console.log('Express server running on port 3000')
