8081f949ecdf0f5005d11a79fa0fa02e70ebca66
knowledge <knowledgedzumba@gmail.com>
2019-04-20 12:56:09 +0200

    added routes for post requests

--- classRoutes.js
+++ classRoutes.js
@@ -22,3 +22,26 @@ router.get('/delete', function (req, res) {
 router.post('/edit', function (req, res) {
   res.sendFile(path.join(__dirname, 'views', 'class', 'edit.html'))
 })
+
+// RESTful api
+router.get('/api/list', function (req, res) {
+  res.json(classList) // Respond with JSON
+})
+
+router.get('/api/get/:id', function (req, res) {
+  res.json(classList[req.params.id])
+})
+
+router.post('/api/create', function (req, res) {
+  console.log('creating a student entry')
+})
+
+router.post('api/delete', function (req, res) {
+  console.log('deleting a sttudent entry')
+})
+
+router.post('api/edit', function (req, res) {
+  console.log('editing a student entry')
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 
 // Import the necessary modules for routing and path manipulation
 let express = require('express')
-let mainRouter = require('./mainRoutes')
+
 let app = express()
 
+let mainRouter = require('./mainRoutes')
+let classRouter = require('./classRoutes')
+
 app.use(mainRouter)
+app.use('/class', classRouter)
 let port = process.env.PORT || 3000
 app.listen(port)
 console.log('Express server running on port 3000')
