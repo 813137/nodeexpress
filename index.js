43875f45e0c1c5a284c95308595c5a2d92ce50f6
knowledge <knowledgedzumba@gmail.com>
2019-04-21 10:12:20 +0200

    got rid of nonsensical garbagge router'

--- index.js
+++ index.js
@@ -7,12 +7,10 @@ let app = express()
 
 // lading body parser
 let bodyParser = require('body-parser')
-let mainRouter = require('./mainRoutes')
 let classRouter = require('./classRoutes')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use('/', mainRouter)
 app.use('/class', classRouter)
 app.use('/cdn', express.static('public'))
 
